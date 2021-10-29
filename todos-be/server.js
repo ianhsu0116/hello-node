c595c755e47f6f95c674ff88a621c7410e4f04e2
IanHsu <ianhsu0116@gmail.com>
2021-10-29 16:33:08 +0800

    20211029 pm

--- todos-be/server.js
+++ todos-be/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Hit this middleware!!!");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Homepage");
+});
+app.get("/cart", (req, res) => {
+  res.send("cart page");
+});
+app.get("/about", (req, res) => {
+  res.send("about me page");
+});
+
+app.use((req, res, next) => {
+  res.send("404 page not found!!!");
 });
 
 app.listen(3502, () => {
